07ebcf850a31348cde032f4e267fbc2360700fea
Mykhailo Meleshchuk <dyugran@gmail.com>
2020-03-06 14:11:55 +0200

    Add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let newArr = [];
+  let count = 0;
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (str[i] === bracketsConfig[j][0] && bracketsConfig[j][0] !== bracketsConfig[j][1]) {
+        newArr.push(str[i])
+      }
+
+      if (str[i] === bracketsConfig[j][1] && bracketsConfig[j][0] !== bracketsConfig[j][1]) {
+        if (newArr[newArr.length - 1] === bracketsConfig[j][0]) {
+          newArr.pop()
+        } else {
+          return false;
+        }
+      }
+
+      if (str[i] === bracketsConfig[j][0] && bracketsConfig[j][0] === bracketsConfig[j][1]) {
+        if (newArr[newArr.length - 1] !== bracketsConfig[j][0]) {
+          newArr.push(str[i]);
+        } else if (newArr[newArr.length - 1] === bracketsConfig[j][0]) {
+          newArr.pop();
+        } else {
+          return false;
+        }
+      }
+    }
+  }
+
+  return newArr.length === 0 ? true : false;
 }
